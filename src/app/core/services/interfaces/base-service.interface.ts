f47aa1adf07cd8aa5550c76b7de6f67c2063cd5d
pablito04 <pablocavaz2004@gmail.com>
2024-10-17 17:53:21 +0200

    Comment

--- src/app/core/services/interfaces/base-service.interface.ts
+++ src/app/core/services/interfaces/base-service.interface.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs';
 import { Paginated } from '../../models/paginated.model';
 
 export interface IBaseService<T> {
-  getAll(page:number, pageSize:number): Observable<Paginated<T>>;
+  getAll(page:number, pageSize:number): Observable<Paginated<T>>;//Devueleve un observable generico 
   getById(id: string): Observable<T | null>;
   add(entity: T): Observable<T>;
   update(id: string, entity: T): Observable<T>;
